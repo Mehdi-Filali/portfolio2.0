05b6033a46550c84658a4c30664cc7b7bc040c14
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-11-14 12:55:22 +0100

    skill & skills props changed

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -6,7 +6,7 @@ import Logo from "../images/logo.png";
 
 type Props = {
   id: number;
-  icon: JSX.Element | string;
+  icon: string | JSX.Element;
   skillName: string;
   exp: string;
   years: number;
