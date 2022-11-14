22b47f2d34848a8b6be76d85b5620a929c209921
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-11-14 15:15:02 +0100

    fixing responsive experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -22,7 +22,7 @@ function Experience({}: Props) {
       transition={{ duration: 1.5 }}
       className="h-screen flex flex-col relative text-left md:flex-row max-w-full px-8 md:px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
 
