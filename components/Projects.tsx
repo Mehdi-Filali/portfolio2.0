697f5d2b73d6edc1daed063565e979a8f7cc631c
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-11-16 15:03:15 +0100

    fixing responsive projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,7 +24,7 @@ export default function Projects({}: Props) {
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projectsData.map((project, index) => (
           <div
-            className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center p-20 md:p-44 h-screen"
+            className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center p-8 md:p-44 h-screen"
             key={project.id}
           >
             <motion.div
@@ -39,13 +39,13 @@ export default function Projects({}: Props) {
               </div>
 
               <div className="space-y-4 px-0 md:px-10 max-w-6xl">
-                <h4 className="text-4xl font-semibold text-center">
+                <h4 className="text-xl md:text-4xl font-semibold text-center">
                   <span className="italic text-[#F7AB0A]/50">
                     Projet {index + 1} sur {projects.length}:
                   </span>{" "}
                   {project.projectName}
                 </h4>
-                <p className="text-lg text-center md:text-left">
+                <p className="text-sm md:text-lg text-center md:text-left">
                   {project.description}
                 </p>
                 <Link href={project.link}>
