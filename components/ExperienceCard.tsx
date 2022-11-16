ef254d047e3d8f54908524b38adeea8b6e0d2da6
Filali Mehdi <aniq.filali.mehdi@gmail.com>
2022-11-16 14:02:32 +0100

    fixing responsive about

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -32,7 +32,7 @@ export default function ExperienceCard({
       key={id}
       className="flex flex-col items-center space-y-7 flex-shrink-0 w-full bg-[#2f2f2f] border-x-2 border-x-[#F7AB0A]/20 p-3 md:p-10"
     >
-      <div className="relative w-32 h-32 rounded-full overflow-hidden xl:w-[200px] xl:h-[200px] border-1 border-black shadow-md bg-white">
+      <div className="relative w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden xl:w-[200px] xl:h-[200px] border-1 border-black shadow-md bg-white">
         <Image
           src={picture}
           alt="Photo about"
@@ -46,9 +46,9 @@ export default function ExperienceCard({
         <h4 className="text-xl md:text-4xl font-light">{jobName}</h4>
         <p className="md:text-2xl text-lg font-bold mt-1">{companyName}</p>
 
-        <p className="uppercase py-5 text-gray-400">{date}</p>
+        <p className="uppercase py-3 md:py-5 text-gray-400">{date}</p>
 
-        <ul className="list-disc space-y-4 ml-5 text-lg">
+        <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
           {missions.map((item) => (
             <li key={id}>{item}</li>
           ))}
